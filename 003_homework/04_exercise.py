4e5f2b3a69a0399a40c0e14915f67c6d3e23050a
Molyanov Vladislav <molyanov1488@gmail.com>
2022-12-06 23:34:29 +0400

    change of solution in problem 03_exercise

--- 003_homework/04_exercise.py
+++ 003_homework/04_exercise.py
@@ -6,12 +6,6 @@
 - 3 -> 11
 - 2 -> 10'''
 
-number10 = int(input('Введите число:'))
+number = bin(int(input('Введите число:')))[2:]
 
-number2 = ''
-
-while number10 > 0:
-    number2 = str(number10 % 2) + number2
-    number10 = number10 // 2
-
-print(number2)
+print(number)
