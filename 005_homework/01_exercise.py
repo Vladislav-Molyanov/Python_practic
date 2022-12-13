6483178233e917b00e74f96c4ce3f4f753b00836
Molyanov Vladislav <molyanov1488@gmail.com>
2022-12-13 20:06:38 +0400

    added problem solution to folder 005_homework

--- 005_homework/01_exercise.py
+++ 005_homework/01_exercise.py
@@ -1,2 +1,6 @@
 '''Напишите программу, удаляющую из текста все слова, содержащие ""абв"".'''
 
+text = "sdfsdf sdf sdf sdfsfd sdfs абв абв ываыва абв ывавыаыв"
+
+result_text = ''.join(filter(lambda x: 'абв' not in x, text.split()))
+print(result_text)
