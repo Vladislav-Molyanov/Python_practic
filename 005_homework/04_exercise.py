e08b87c97c52cdd5fb35dafe416654918dff517e
Molyanov Vladislav <molyanov1488@gmail.com>
2022-12-16 17:17:23 +0400

    added problem solution 04_exercise to folder 005_homework

---
+++ 005_homework/04_exercise.py
@@ -0,0 +1,24 @@
+'''Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.'''
+
+
+def rle_encode(data):
+    encoding = ''
+    prev_char = ''
+    count = 1
+    if not data:
+        return ''
+    for char in data:
+        if char != prev_char:
+            if prev_char:
+                encoding += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        encoding += str(count) + prev_char
+        return encoding
+
+
+encoded_val = rle_encode('AAAAAAFDDCCCCCCCAEEEEEEEEEEEEEEEEE')
+print(encoded_val)
